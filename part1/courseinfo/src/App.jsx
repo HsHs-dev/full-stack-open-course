565061b289a98727ac4fd4dde22907867b541586
Hassan Mahmoud <officialhassansmail@gmail.com>
2025-08-11 12:23:59 +0300

    - solve part1 exercises 1.3.-1.5.

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,16 +1,18 @@
 const App = () => {
-  const parts = [
-    {part: 'Fundamentals of React', exercise: 10},
-    {part: 'Using props to pass data', exercise: 7},
-    {part: 'State of a component', exercise: 14} 
-  ]
-  const course = 'Half Stack application development'
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {part: 'Fundamentals of React', exercises: 10},
+      {part: 'Using props to pass data', exercises: 7},
+      {part: 'State of a component', exercises: 14} 
+    ]
+ }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total exercises={parts.map(p => p.exercise)} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
@@ -26,9 +28,9 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.parts[0].part} exercise={props.parts[0].exercise} />
-      <Part part={props.parts[1].part} exercise={props.parts[1].exercise} />
-      <Part part={props.parts[2].part} exercise={props.parts[2].exercise} />
+      <Part part={props.parts[0].part} exercise={props.parts[0].exercises} />
+      <Part part={props.parts[1].part} exercise={props.parts[1].exercises} />
+      <Part part={props.parts[2].part} exercise={props.parts[2].exercises} />
     </div>
   )
 }
@@ -36,12 +38,13 @@ const Content = (props) => {
 const Part = (props) => {
   return (
     <p>
-      {props.part}{props.exercise}
+      {props.part} {props.exercise}
     </p>
   )
 }
 
-const Total = ({exercises}) => {
+const Total = (props) => {
+  const exercises = props.parts.map(p => p.exercises)
   const total = exercises.reduce((sum, num) => sum + num, 0)
   return (
     <p>
