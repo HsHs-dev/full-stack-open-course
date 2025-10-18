09d3fd35746dbbfe7bea0075757df213f25f6751
Hassan Mahmoud <officialhassansmail@gmail.com>
2025-10-18 13:27:19 +0300

    - link the notes backend to frontend

--- part3/notes/index.js
+++ part3/notes/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 const requestLogger = (req, res, next) => {
   console.log('Method:', req.method)
@@ -11,6 +12,8 @@ const requestLogger = (req, res, next) => {
 
 app.use(express.json())
 app.use(requestLogger)
+app.use(cors())
+app.use(express.static('dist'))
 
 let notes = [
   {
@@ -90,7 +93,7 @@ const unknownEndpoint = (req, res) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
